87158ba1c6a6850c660afb9a61631d80561c3d7d
Darian Sampare <dsampare04@gmail.com>
2019-08-22 13:18:09 -0700

    initial commit, grid and navbar components started

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                {/* <img src="" width="30" height="30" class="d-inline-block align-top" alt=""> */}
+                SearchBuddy
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar;
